477a461efd1ca0cf456029cdc8f64eba67889481
Eric Woolard <premierproductions601@gmail.com>
2017-08-29 11:48:48 -0500

    Removed icon_emoji argument from chat.postMessage req

--- call_home.py
+++ call_home.py
@@ -11,7 +11,7 @@ slack_client = SlackClient(settings['stickybot']['token'])
 def sendAlert(url):
     message = '<!here> *HEYO!* Someone just used me to sticky a post on r/{}. I hope it was okay... Will you check for me? :sadsmiley: \n{}'.format(settings['reddit']['subreddit'], url)
     try:
-        slack_client.api_call("chat.postMessage", channel=settings['call_home_channel'], text=message, as_user=False, icon_emoji=':stickybot:', username='stickybot')
+        slack_client.api_call("chat.postMessage", channel=settings['call_home_channel'], text=message, as_user=False, username='stickybot')
     except:
         req_error = traceback.format_exc()
         now = str(datetime.now())
